dba0747ac01d84ca5e55bfd853ebd1fd4dc5779f
Gane <1970722893@qq.com>
2026-05-16 21:35:39 +0800

    🎀 七七的粉色城堡

---
+++ sw.js
@@ -0,0 +1,32 @@
+// 七七的粉色城堡 - Service Worker
+const CACHE = 'pink-castle-v1';
+const URLS = [
+  '/',
+  '/index.all-in-one.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(URLS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).catch(function() { return r; });
+    })
+  );
+});
